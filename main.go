219cc1418b6b1177be7395c03e9437f9acfaca48
20zinnm <meyerzinn@gmail.com>
2017-04-26 20:47:37 -0500

    Foundation is working! Time to wire stuff up.

--- main.go
+++ main.go
@@ -3,14 +3,50 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/logging/logrus"
 	"github.com/goadesign/goa/middleware"
+	_ "github.com/lib/pq"
+	"github.com/rubenv/sql-migrate"
 	"github.com/studiously/core/app"
+	"github.com/studiously/core/ddl"
 )
+
+const DatabaseKey = "database"
 
 func main() {
 	// Create service
 	service := goa.New("studiously")
+	service.WithLogger(goalogrus.New(logrus.StandardLogger()))
+
+	var driver = os.Getenv("DATABASE_DRIVER")
+	var config = os.Getenv("DATABASE_CONFIG")
+
+	db, err := sql.Open(driver, config)
+	if err != nil {
+		logrus.Fatalln("database connection failed", err)
+	}
+	if err := pingDatabase(db); err != nil {
+		logrus.Errorln(err)
+		logrus.Fatalln("database ping attempts failed")
+	}
+	if err := setupDatabase(driver, db); err != nil {
+		logrus.Errorln(err)
+		logrus.Fatalln("migration failed")
+	}
+	service.Use(func(h goa.Handler) goa.Handler {
+		return func(ctx context.Context, rw http.ResponseWriter, req *http.Request) error {
+			ctx = context.WithValue(ctx, DatabaseKey, db)
+			return h(ctx, rw, req)
+		}
+	})
 
 	// Mount middleware
 	service.Use(middleware.RequestID())
@@ -30,4 +66,28 @@ func main() {
 		service.LogError("startup", "err", err)
 	}
 
+}
+
+// helper function to setup the databsae by performing
+// automated database migration steps.
+func setupDatabase(driver string, db *sql.DB) error {
+	var migrations = &migrate.AssetMigrationSource{
+		Asset:    ddl.Asset,
+		AssetDir: ddl.AssetDir,
+		Dir:      driver,
+	}
+	_, err := migrate.Exec(db, driver, migrations, migrate.Up)
+	return err
+}
+
+func pingDatabase(db *sql.DB) (err error) {
+	for i := 0; i < 30; i++ {
+		err = db.Ping()
+		if err == nil {
+			return
+		}
+		logrus.Infof("database ping failed. retry in 1s")
+		time.Sleep(time.Second)
+	}
+	return
 }
